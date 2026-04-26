34ff825d130b9b86dc82d65c82d5548d060a75e5
Necdet Yavuz AKTAS <aktasnecdetyavuz@gmail.com>
2026-04-26 17:34:37 +0300

    NextAuth giriş sistemi tamamlandı

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 const prisma = new PrismaClient();
 
