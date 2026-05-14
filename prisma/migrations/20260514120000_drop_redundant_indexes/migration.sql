0f25669f6bcf3f8be95a9b8e85ece8711c78fb50
Necdet Yavuz AKTAS <aktasnecdetyavuz@gmail.com>
2026-05-14 08:56:54 +0300

    Redundant DB indekslerini kaldir

---
+++ prisma/migrations/20260514120000_drop_redundant_indexes/migration.sql
@@ -0,0 +1,6 @@
+DROP INDEX IF EXISTS "Davetiye_aktif_idx";
+DROP INDEX IF EXISTS "Davetiye_userId_idx";
+DROP INDEX IF EXISTS "Davetli_davetiyeId_idx";
+DROP INDEX IF EXISTS "MedyaSilmeKuyrugu_sonrakiDeneme_idx";
+DROP INDEX IF EXISTS "Masa_davetiyeId_idx";
+DROP INDEX IF EXISTS "RSVP_davetiyeId_idx";
